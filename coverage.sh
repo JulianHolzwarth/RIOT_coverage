a3d65ed3976cb1db2312fc53db2d7123daecc3ca
JulianHolzwarth <julian.holzwarth@fu-berlin.de>
2023-10-11 15:34:20 +0200

    Add Coverage.sh file

---
+++ coverage.sh
@@ -0,0 +1,21 @@
+source ./scripts/scipt.sh
+
+Output_sum() {
+  cat ../Projekt/$1/ausgabe.txt | sed "s/lines: /$1;/" | sed "s/branches: //" | sed "s/% (/;/" | sed "s/ out of /;/"| sed "s/)//" | sed "s/0.0;0;0/100.0;0;0/" | tr '\n' ';'| sed 's/.$//' >> daten.txt
+  echo "" >> daten.txt
+}
+
+echo Starting coverage
+
+
+./scripts/mutex.sh &
+./scripts/sema.sh &
+./scripts/cond.sh &
+./scripts/event.sh &
+./scripts/evtimer.sh &
+./scripts/msg.sh &
+./scripts/threadflag.sh &
+
+wait
+echo done coverage
+
