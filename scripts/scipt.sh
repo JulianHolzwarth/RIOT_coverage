0ed88938a71d3e84e409850d173f19edc79abdcf
JulianHolzwarth <julian.holzwarth@fu-berlin.de>
2023-10-12 15:00:52 +0200

    Added comments and better variable names

--- scripts/scipt.sh
+++ scripts/scipt.sh
@@ -1,14 +1,18 @@
+#!/bin/bash
 
+# Function to compile a single test
 compile_test() {
-  LANG=C.UTF-8 BOARD=native make -C tests/$1/$2/ > /dev/null 2>&1
-  #LANG=C.UTF-8 BOARD=native make -C tests/$1/ test > /dev/null 2>&1
-  echo s | ./tests/$1/$2/bin/native/tests_$2.elf > /dev/null 2>&1 &
+  local category="$1"
+  local test_name="$2"
+  LANG=C.UTF-8 BOARD=native make -C "tests/$category/$test_name/" > /dev/null 2>&1
+  echo s | "./tests/$category/$test_name/bin/native/tests_$test_name.elf" > /dev/null 2>&1 &
   pid=$!
   sleep 10
   kill -SIGINT $pid
-  echo $2
+  echo "Compiled and tested: $test_name"
 }
 
+# Function to compile multiple tests
 compile_tests() {
   declare -a tests=("${!2}")
   for i in ${!tests[@]}; do
@@ -20,16 +24,15 @@ cleaning() {
   LANG=C.UTF-8 BOARD=native make -C tests/$1/ clean > /dev/null 2>&1
 }
 
+# Function to clean a single test
 clean() {
   LANG=C.UTF-8 BOARD=native make -C tests/$1/$2/ clean > /dev/null 2>&1
 }
 
+# Function to clean multiple tests
 clean_multi() {
   declare -a tests=("${!2}")
   for i in ${!tests[@]}; do
     clean $1 ${tests[$i]} &
   done
 }
-
-#ps -aux | grep .elf | awk '{print $2}' | kill -9
-#cat ausgabe.txt | sed "s/.*: //" |sed "s/%.*(/;/" | sed "s/ out of /;/"| sed "s/)/;/" | tr '\n' ' '
